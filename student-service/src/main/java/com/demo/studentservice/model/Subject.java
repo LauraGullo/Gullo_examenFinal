3d221309abb74dbe6a5fb61d57f9b0b7023a86e6
LauraGullo <marialgullo@gmail.com>
2022-07-15 11:18:05 -0300

    Config added

--- student-service/src/main/java/com/demo/studentservice/model/Subject.java
+++ student-service/src/main/java/com/demo/studentservice/model/Subject.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Subject {
+    private int id;
     private String name;
     private String professor;
     private int studentId;
